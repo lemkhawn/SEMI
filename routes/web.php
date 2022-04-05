a3d678116c77c25e42c9705ce12e73c96bda09d5
lemkhawn <khanhnlbhbf200025@fpt.edu.vn>
2022-04-05 19:50:08 +0700

    LK commit2

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,30 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/index', function () {
+    return view('index');
+});
+Route::get('/products', function () {
+    return view('products');
+});
+Route::get('/contact', function () {
+    return view('contact');
+});
+Route::get('/blog', function () {
+    return view('blog');
+});
+Route::get('/about', function () {
+    return view('about');
+});
+Route::get('/cart', function () {
+    return view('cart');
+});
+Route::get('/chair', function () {
+    return view('chair');
+});
+Route::get('/tab', function () {
+    return view('table');
+});
+Route::get('/chan', function () {
+    return view('chandeliers');
 });
