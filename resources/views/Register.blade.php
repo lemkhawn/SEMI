9fc5f141feb30a8b456257399cf83ea7e9abcc07
quangxi <98067518+quangxi@users.noreply.github.com>
2022-04-06 02:45:39 +0700

    quang1

--- resources/views/Register.blade.php
+++ resources/views/Register.blade.php
@@ -1,38 +1,38 @@
-@include('Header')
+@include('header')
 @yield('Register')
 <body>
-  <div class="container">
+  <div class="container-r">
     <div class="title">Registration</div>
-    <div class="content">
+    <div class="content-r">
       <form action="#">
-        <div class="user-details">
-          <div class="input-box">
+        <div class="user-details-r">
+          <div class="input-box-r">
             <span class="details">Full Name</span>
             <input type="text" placeholder="Enter your name" required>
           </div>
-          <div class="input-box">
+          <div class="input-box-r">
             <span class="details">Username</span>
             <input type="text" placeholder="Enter your username" required>
           </div>
-          <div class="input-box">
+          <div class="input-box-r">
             <span class="details">Email</span>
             <input type="text" placeholder="Enter your email" required>
           </div>
-          <div class="input-box">
+          <div class="input-box-r">
             <span class="details">Phone Number</span>
             <input type="text" placeholder="Enter your number" required>
           </div>
-          <div class="input-box">
+          <div class="input-box-r">
             <span class="details">Password</span>
             <input type="text" placeholder="Enter your password" required>
           </div>
         </div>
-        <div class="gender-details">
+        <div class="gender-details-r">
           <input type="radio" name="gender" id="dot-1">
           <input type="radio" name="gender" id="dot-2">
           <input type="radio" name="gender" id="dot-3">
           <span class="gender-title">Gender</span>
-          <div class="category">
+          <div class="category-r">
             <label for="dot-1">
             <span class="dot one"></span>
             <span class="gender">Male</span>
@@ -40,6 +40,10 @@
           <label for="dot-2">
             <span class="dot two"></span>
             <span class="gender">Female</span>
+          </label>
+          <label for="dot-3">
+            <span class="dot three"></span>
+            <span class="gender">Other</span>
           </label>
           </div>
         </div>
